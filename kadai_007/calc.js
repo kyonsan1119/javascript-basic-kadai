532a5d226d4e45f332403cb1836e914bb1322cc8
kyonsan1119 <k.yuki1119@gmail.com>
2023-08-05 11:16:07 +0900

    kadai_007修正

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,17 +1,12 @@
-let num = Math.floor(Math.random() * 100)
+let num = Math.floor(Math.random() * 30)
 
-if (num % 3 === 0) {
-  console.log("3の倍数です")
-}
-
-if (num % 5 === 0) {
-  console.log("5の倍数です")
-}
-
-if (num % 15 === 0) {
-  console.log("3と5の倍数です")
-}
-
-else {
-  console.log(num)
+// 条件分岐による出力
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log("3と5の倍数です");
+} else if (num % 3 === 0) {
+  console.log("3の倍数です");
+} else if (num % 5 === 0) {
+  console.log("5の倍数です");
+} else {
+  console.log(num);
 }
